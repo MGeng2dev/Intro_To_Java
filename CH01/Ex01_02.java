c067375a3549cbc2245c7163a2cbaf03b38efa12
Maris <mkgpec@gmail.com>
2021-06-12 11:57:17 -0400

    initial commit

---
+++ CH01/Ex01_02.java
@@ -0,0 +1,14 @@
+//  Display five messages
+//  Write a porgram that displays
+//  Welcome to Java five times
+
+public class Ex01_02 {
+   public static void main(String[] args) {
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      System.out.println("Welcome to Java");
+      
+   }
+}
