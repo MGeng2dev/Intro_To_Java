9a2ae9fd950c55dd9602be74a430fb61df5c04d8
Maris <mkgpec@gmail.com>
2021-06-16 09:42:52 -0400

    initial commit

---
+++ CH01/Ex01_08.java
@@ -0,0 +1,14 @@
+//  Area and perimeter of a circle
+//  Write a program that displays the area and perimeter of a circle
+//  that has a radius of 5.5 using the following formulas:
+//  Perimeter = 2 x radius x pi
+//  Area = radius x radius x pi
+
+public class Ex01_08 {
+   public static void main(String[] args) {
+      System.out.println("Circle with radius = 5");
+      System.out.println("Perimeter = 2 x radius x pi = " + 2 * 5.5 * 3.14159);
+      System.out.println("Area = radius x radius x pi = " + 5.5 * 5.5 * 3.14159);
+      
+   }
+}
---
+++ CH01/Ex01_09.java
@@ -0,0 +1,12 @@
+//  Area and perimeter of a rectangle 
+//  Write a program that displays the area and perimeter of a rectangle
+//  with the width = 4.5 and height = 7.5 using the following formula:
+//  Area = width x height
+
+public class Ex01_09 {
+   public static void main(String[] args) {
+      System.out.println("Rectangle with width = 4.5 and height = 7.9");
+      System.out.println("Area = width x height = " + 4.5 * 7.5);
+      
+   }
+}
