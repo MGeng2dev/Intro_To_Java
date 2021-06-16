91c787be5a14f124775b6e34fb42762560f8e171
Maris <mkgpec@gmail.com>
2021-06-16 09:43:56 -0400

    initial commit

---
+++ CH01/Ex01_10.java
@@ -0,0 +1,12 @@
+//  Average speed in miles
+//  Assume a runner runs 14 kilometers in 45 minutes and 30 seconds.
+//  Write a program that displays the average speed in miles per hour. 
+//  Note: 1 miles is 1.6 km
+
+public class Ex01_10 {
+   public static void main(String[] args) {
+      System.out.println("14 kilometers in 45 minutes and 30 seconds");
+      System.out.println("Is equivalent to: " + (14 / 45.5 / 1.6 * 60) + " mph");
+      
+   }
+}
